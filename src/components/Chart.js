41049cd886481e5426719dc3344ca0e4a188a55d
Suri <awumsuri@gmail.com>
2018-08-09 04:09:57 -0400

    Building

--- src/components/Chart.js
+++ src/components/Chart.js
@@ -12,26 +12,45 @@ class Chart extends PureComponent {
         this.page = 1;
         this.data = []
         this.onClickPage = this.onClickPage.bind(this)
+        this.onDataProviderChange = this.onDataProviderChange.bind(this)
+        this.onPageSizeChange = this.onPageSizeChange.bind(this)
+        this.pageSize = 15
+        this.cache = new Map()        
     }
     
     state = {
         dataProvider: undefined,
-        pageSize: 15
+    }
+
+    onPageSizeChange(event) {
+        this.pageSize = parseInt(event, 10)
+        this.showPage()
+    }
+
+    onDataProviderChange(key) {
+        switch(key) {
+            case "2":
+                this.loadData("numbers.csv")
+                break
+            case "1":
+                this.loadData("people.csv")
+                break
+        }
     }
 
     onClickPage(event) {
         const { name } = event.target
-        const { pageSize } = this.state
+        const { pageSize, data, currentIndex } = this
         let pageCheck = 0;
 
         switch(name) {
             case "Back":
-                pageCheck = this.currentIndex - pageSize
+                pageCheck = currentIndex - pageSize
                 this.currentIndex = pageCheck > 0 ? pageCheck : 1
             break;                
             case "Forward":
-                pageCheck = this.currentIndex + pageSize
-                this.currentIndex = pageCheck > this.data.length-1 ? this.currentIndex : pageCheck 
+                pageCheck = currentIndex + pageSize
+                this.currentIndex = pageCheck > data.length-1 ? currentIndex : pageCheck 
             break;                
         }
 
@@ -39,16 +58,15 @@ class Chart extends PureComponent {
     }
 
     showPage() {
-        const { pageSize } = this.state
-
+        const { data, currentIndex, pageSize } = this
         this.setState({
-            dataProvider: this.data.slice(this.currentIndex, this.currentIndex + pageSize)
+            dataProvider: data.slice(currentIndex, currentIndex + pageSize)
         })
     }
 
-    parseData(rawData) {
+    parseData(url, rawData) {
         this.data = parser.parseCSV(rawData)
-
+        this.cache.set(url, this.data)
         this.showPage()
     }
 
@@ -57,10 +75,16 @@ class Chart extends PureComponent {
             "Content-Type": "text/csv"
         })
 
+        if (this.cache.has(url)) {
+            this.data = this.cache.get(url)
+            this.showPage()
+            return
+        }
+
         fetch(url, headers)
             .then((rawData) => rawData.text())
             .then((data) => {
-                this.parseData(data)
+                this.parseData(url, data)
             })
             .catch((err) => console.error(err))
     }
@@ -75,9 +99,12 @@ class Chart extends PureComponent {
 
         return (
             <div>
-                <Menu />
+                <Menu 
+                    onDataProviderChange={this.onDataProviderChange} 
+                    onPageSizeChange={this.onPageSizeChange}
+                />
                 <BootstrapTable data={dataProvider} stripe hover>
-                    <TableHeaderColumn isKey dataField="id">ID</TableHeaderColumn>
+                    <TableHeaderColumn isKey width="70" dataField="id">ID</TableHeaderColumn>
                     {
                         template.map(
                             (data) => 
--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,23 +1,21 @@
 import React, { PureComponent } from 'react'
 import { SplitButton, MenuItem } from 'react-bootstrap'
 
-class Menu extends PureComponent {
-    state = {
-        csv: undefined
-    }
-
-
-    render() {
-        return (
-            <div className="menu">
-                <SplitButton title="Data Provider" bsStyle="info">
-                    <MenuItem eventKey="1">People</MenuItem>
-                    <MenuItem eventKey="2">Financial</MenuItem>
-                </SplitButton>
-            
-            </div>
-        )
-    }
-} 
+const Menu = (props) => (
+    <div className="menu">
+        <SplitButton title="Page Size" bsStyle="info" key="1" onSelect={e => props.onPageSizeChange(e)}>
+            <MenuItem eventKey="15">15</MenuItem>
+            <MenuItem eventKey="30">30</MenuItem>
+            <MenuItem eventKey="60">60</MenuItem>
+            <MenuItem eventKey="75">75</MenuItem>
+            <MenuItem eventKey="90">90</MenuItem>
+            <MenuItem eventKey="120">120</MenuItem>
+        </SplitButton>
+        <SplitButton title="Data Provider" bsStyle="info" key="2" onSelect={e => props.onDataProviderChange(e)}>
+            <MenuItem eventKey="1">People</MenuItem>
+            <MenuItem eventKey="2">Financial</MenuItem>
+        </SplitButton>
+    </div>
+)
 
 export default Menu
